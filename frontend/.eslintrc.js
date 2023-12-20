871fcf17f35330ab3e12c150abbb07773b5b7c4b
DanniGudna <dannigudna@gmail.com>
2023-12-20 22:11:33 +0000

    Removed cypress

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,7 +2,6 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true, // Jest global variables like `it` etc.
     node: true, // Defines things like process.env when generating through node
   },
   extends: [
@@ -12,7 +11,6 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:jsx-a11y/recommended',
     'plugin:react-hooks/recommended',
-    'plugin:jest/recommended',
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',

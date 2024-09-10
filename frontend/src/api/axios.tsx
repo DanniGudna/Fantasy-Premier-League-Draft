b2bc8d7c19ec5a7bc2b29e577dfda201cc885bce
DanniGudna <dannigudna@gmail.com>
2024-09-10 15:21:12 +0000

    updated with previous season data

--- frontend/src/api/axios.tsx
+++ frontend/src/api/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: 'http://localhost:3001/', // Todo add a env file for this
+  //baseURL: 'http://localhost:3001/', // Todo add a env file for this
   baseURL: 'https://fpl-server.glitch.me/',
 
   headers: {
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,22 +1,20 @@
 /* eslint-disable global-require */
-import { white } from 'tailwindcss/colors';
+import { white } from "tailwindcss/colors";
 /** @type {import('tailwindcss').Config} */
-export const content = ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'];
-export const darkMode = 'class';
+export const content = ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"];
+export const darkMode = "class";
 export const theme = {
   extend: {
     colors: {
       background: white,
-      'darkmode-background': '#475577', // slate-600
-      text: '#1a202c', // gray-900
-      'darkmode-text': white,
-      'sub-text': '#6b7280', // gray-500
+      "darkmode-background": "#475577", // slate-600
+      text: "#1a202c", // gray-900
+      "darkmode-text": white,
+      "sub-text": "#6b7280", // gray-500
       // 'darkmode-sub-text': , // ??
       main: white, // TODO maybe change
-      'darkmode-main': '#0F172A', // slate-950
+      "darkmode-main": "#0F172A", // slate-950
     },
   },
 };
-export const plugins = [
-  require('tailwind-scrollbar'),
-];
+export const plugins = [require("tailwind-scrollbar")];
